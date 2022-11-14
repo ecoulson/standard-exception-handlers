0086d6fb8e6db776b4f4dc01315607719aaa75a1
Evan Coulson <evan.m.coulson@gmail.com>
2022-11-14 13:56:50 -0800

    Fixed build error

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
+import { Action } from '@the-standard/types';
 import { ExceptionHandlingClient } from './clients/exception-handling/exception-handling-client';
-import { AsyncFunction } from './models/exception-handling/async-function';
-import { Function } from './models/exception-handling/function';
 
-export function tryCatch<T>(func: Function<T>) {
+export function tryCatch<T>(func: Action<T>) {
     const client = new ExceptionHandlingClient<T>();
     return client.tryCatch(func);
 }
 
-export function tryCatchAsync<T>(func: AsyncFunction<T>) {
+export function tryCatchAsync<T>(func: Action<Promise<T>>) {
     const client = new ExceptionHandlingClient<T>();
     return client.tryCatchAsync(func);
 }
